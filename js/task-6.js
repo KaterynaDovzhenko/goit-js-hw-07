c4274c8a342a1b2d669d3dadef3ad1b021e90b15
Kate <kate.dovzhenk@gmail.com>
2024-12-22 16:28:54 -0500

    done

---
+++ js/task-6.js
@@ -0,0 +1,41 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const input = document.querySelector("input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+  boxes.innerHTML = "";
+
+  const elements = [];
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    size += 10;
+    box.style.backgroundColor = getRandomHexColor();
+    elements.push(box);
+  }
+  boxes.append(...elements);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
+createBtn.addEventListener("click", () => {
+  const amount = Number(input.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = "";
+  }
+});
+
+destroyBtn.addEventListener("click", destroyBoxes);
