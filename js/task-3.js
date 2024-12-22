b0a7c2d409b66312964ce04b286ae565c36cf3a0
Kate <kate.dovzhenk@gmail.com>
2024-12-22 16:12:04 -0500

    js module 8 hw

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const fieldInput = document.querySelector("#name-input");
-const fieldOutput = document.querySelector("#name-output");
-
-fieldInput.addEventListener("input", () => {
-  const trimmedValue = fieldInput.value.trim();
-  fieldOutput.textContent = trimmedValue === "" ? "Annonymus" : trimmedValue;
-});
